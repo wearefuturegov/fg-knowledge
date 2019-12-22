147be5021eb220a3b82b7b0bdfea18025250f9b7
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-22 20:35:50 +0000

    markdown parser set up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    def markdown(text)
+        renderer = ::Redcarpet::Render::HTML.new({
+            hard_wrap: true,
+        })
+        markdown = ::Redcarpet::Markdown.new(renderer)
+        markdown.render(text).html_safe
+    end
 end
