d41ad7f2cb3a878e403d027d6453a5bfcbec7c07
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-22 16:06:44 +0000

    home page done

---
+++ app/uploaders/fae/image_uploader.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+module Fae
+    class ImageUploader < CarrierWave::Uploader::Base
+  
+      include CarrierWave::MiniMagick
+  
+      # saves file size to DB
+      process :save_file_size_in_model
+      def save_file_size_in_model
+        model.file_size = file.size
+      end
+  
+      def extension_whitelist
+        %w(jpg jpeg gif png ico)
+      end
+  
+      # Override the directory where uploaded files will be stored.
+      # This is a sensible default for uploaders that are meant to be mounted:
+      def store_dir
+        "system/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+      end
+  
+      version :thumb do
+        process :resize_to_fill => [150,100]
+      end
+
+      version :home do
+        process :resize_to_fill => [250,167]
+      end
+  
+    end
+  end
+  
