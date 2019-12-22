f23e55f169a58c00d7471be1820f0a140303e14d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-22 20:08:54 +0000

    add recent changes and some basic page content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,22 @@
 class HomeController < ApplicationController
     def index
-        @teams = Team.all
-        @isHome = true
+        @teams = Team.order(:name)
+        # home page layout
+        @is_home = true
+        @recently_updated = recently_updated
     end
+
+    private
+
+    def recently_updated(num=3)
+        list = []
+        [Guide, Artefact].each do |m|
+            list << m.all.sort_by(&:updated_at).flatten
+        end
+        list.flatten
+            .sort_by(&:updated_at)
+            .reverse
+            .first(num)
+    end
+
 end
