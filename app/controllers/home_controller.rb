484c0cc0ec440369d001aa3b2187bb9e886b86cf
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-23 11:33:15 +0000

    style up guides sidebar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,19 +8,14 @@ class HomeController < ApplicationController
     private
 
     def recently_updated(num=4)
-
-        guides = Guide.order(:updated_at).limit(4)
-        artefacts = Artefact.order(:updated_at).limit(4)
-        [guides, artefacts].flatten.sort_by{ |r| r[:updated_at] }.reverse.first(num)
-
-        # list = []
-        # [Guide, Artefact].each do |m|
-        #     list << m.all.sort_by(&:updated_at).flatten
-        # end
-        # list.flatten
-        #     .sort_by(&:updated_at)
-        #     .reverse
-        #     .first(num)
+        list = []
+        [Guide, Artefact].each do |m|
+            list << m.all.sort_by(&:updated_at).flatten
+        end
+        list.flatten
+            .sort_by(&:updated_at)
+            .reverse
+            .first(num)
     end
 
 end
