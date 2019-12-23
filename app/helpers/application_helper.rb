d2154192f0254c29c5181f2af9f81a6e6a697c3d
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-23 00:11:04 +0000

    add automatic contents to guides

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,18 @@ module ApplicationHelper
     def markdown(text)
         renderer = ::Redcarpet::Render::HTML.new({
             hard_wrap: true,
+            with_toc_data: true
         })
         markdown = ::Redcarpet::Markdown.new(renderer, {
             autolink: true
         })
         markdown.render(text).html_safe
+    end
+
+
+    def toc(text)
+        renderer = ::Redcarpet::Render::HTML_TOC.new()
+        markdown = ::Redcarpet::Markdown.new(renderer)
+        markdown.render(text)
     end
 end
