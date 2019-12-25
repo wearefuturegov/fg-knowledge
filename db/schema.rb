71e1f0f5604922322030fdfccb4c207c8ddf1923
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-25 20:42:33 +0000

    reove users table

--- db/schema.rb
+++ db/schema.rb
@@ -211,13 +211,6 @@ ActiveRecord::Schema.define(version: 2019_12_25_193232) do
     t.text "learn_more"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "google_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   add_foreign_key "collections", "teams"
   add_foreign_key "guides", "collections"
 end
