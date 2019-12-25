a437d6c8361b986ead494480f05b3ad9d738d22a
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-25 21:08:24 +0000

    restrict logins to futuregov

---
+++ app/controllers/google_sign_in/authorizations_controller.rb
@@ -0,0 +1,19 @@
+require 'securerandom'
+
+class GoogleSignIn::AuthorizationsController < GoogleSignIn::BaseController
+  skip_forgery_protection only: :create
+
+  def create
+    redirect_to login_url(scope: 'openid profile email', state: state, hd: "wearefuturegov.com"),
+      flash: { proceed_to: params.require(:proceed_to), state: state }
+  end
+
+  private
+    def login_url(**params)
+      client.auth_code.authorize_url(prompt: 'login', **params)
+    end
+
+    def state
+      @state ||= SecureRandom.base64(24)
+    end
+end
